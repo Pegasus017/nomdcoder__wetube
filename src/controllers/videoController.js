8b9525177c440bb59d37737bac0d555ebe986e42
Pegasus017 <makh007@naver.com>
2022-08-25 15:43:47 +0900

    done #5
    made templates of res.render of controllers using pug.
    in the pug rendering templates, used conditions, iteration, mixins.

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,34 @@
-export const trending = (req,res) =>{return res.send(`trending video`)};
-export const see = (req,res) =>{return res.send(`watch video #${req.params.id}`)};
-export const edit = (req,res) => {return res.send(`edit video`)};
-export const search = (req,res) =>{return res.send(`search video`)};
-export const upload =(req,res)=>{return res.send(`upload video`)};
-export const deleteVideo =(req,res)=>{return res.send(`delete video`)};
+export const trending = (req,res) => {
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 60,
+            id: 1,
+        },
+        {
+            title: "Second Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 60,
+            id: 1,
+        },
+        {
+            title: "Third Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 60,
+            id: 1,
+        }
+    ];
+    return res.render("home", {pageTitle: "Home", videos});
+}
+export const watch = (req,res) => res.render("watch");
+export const edit = (req,res) => res.render("edit");
+export const search = (req,res) => res.send(`search video`);
+export const upload =(req,res)=> res.send(`upload video`);
+export const deleteVideo =(req,res)=> res.send(`delete video`);
