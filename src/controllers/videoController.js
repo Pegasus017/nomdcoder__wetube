51bc13592816c87b3a2b61636d1a3b68c60ab5bb
Pegasus017 <makh007@naver.com>
2022-08-29 00:09:58 +0900

    done till #6.17
    started using db on both desktop and laptop

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,8 @@
 import Video from "../models/Video";
 
-//*Video.find({}, (error, videos) => {});*//
-
 export const home = async(req,res) => {
     const videos = await Video.find({});
+    console.log(videos);
     res.render("home", {pageTitle: "Home", videos:[]});
 };
 
@@ -25,9 +24,23 @@ export const getUpload = (req,res) => {
     return res.render("upload", {pageTitle:"Upload Video"});
 }
 
-export const postUpload = (req,res) =>{
-    const {title} = req.body;
-    return res.redirect("/");
+export const postUpload = async (req,res) =>{
+    const {title, description, hashtags} = req.body;
+    try{
+        await Video.create({
+            title,
+            description,
+            createdAt: Date.now(),
+            hashtags: hashtags.split(",").map(word=>`#${word}`),
+        });
+        return res.redirect("/");
+    }catch(error){
+        console.log(error);
+        return res.render("upload", {
+            pageTitle:"Upload Video",
+            errorMessage: error._message,
+        });
+    }
 }
 
 export const search = (req,res) => res.send(`search video`);
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import {upload, watch, getEdit, deleteVideo, postEdit, getUpload, postUpload} from "../controllers/videoController";
+import {watch, getEdit, deleteVideo, postEdit, getUpload, postUpload} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.route("/upload").get(getUpload).post(postUpload);
-videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+
+videoRouter.get("/:id(\\d+)", watch);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 
